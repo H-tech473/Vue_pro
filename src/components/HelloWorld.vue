4897fdc3804c14a827d68f4b725203b92eb0126f
H-tech473 <78791161+H-tech473@users.noreply.github.com>
2023-10-28 15:12:30 +0530

    feedback button and search bar done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,14 @@
         info: [{message:'',name: ''},{message:'',name: ''},{message:'',name: ''},{message:'',name: ''}]
       }
     },
+    methods : {
+ 
+ // Creating function
+    show: function(){
+      var element = document.querySelector("form");
+      element.classList.toggle("d-none");
+        }
+    },
     mounted () {
       axios({
           method: 'get',
@@ -41,13 +49,18 @@
       </ul>
 
       <div class="col-md-3 text-end">
-        <a href="#" class="px-3 mx-1 py-3 svg"><svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a>
+        <a href="#" class="px-3 mx-1 py-3 svg" @click="show()"><svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a>
         <a href="#" class="px-3 mx-1 py-3 svg"><svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg></a>
         <a href="#" class="px-3 mx-1 py-3 svg d-sm-inline d-md-none"><svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></a>
       </div>
     </header>
   </div>
   <div class="flex-grow-1 position-relative" style="overflow-y: auto; overflow-x: hidden;">
+    <button type="button" class="position-fixed btn btn-light" style="z-index: 999; transform: rotateZ(-90deg) translateY(39%); top: 30%; right: 0%;">FEEDBACK</button>
+    <form type="button" class="position-absolute d-none input-group p-3 bg-light" action="/" style="z-index: 999; width: 30%; right: 0%;top: 0%">
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Enter Text">
+      <button type="button" class="btn btn-warning">Search</button>
+    </form>
     <div class="picture position-absolute">
       <div class="pic1 w-50 h-100 d-inline-block position-absolute d-flex align-items-end justify-content-center">
         <div class="w-100 h-60 position-absolute d-flex align-items-center justify-content-center flex-column py-3">
